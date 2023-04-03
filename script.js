cbe4605c465e5b69d61098413b1b0f1f5aa5c81a
lailaa7 <ti.lailafdh7@gmail.com>
2023-04-03 19:25:10 +0700

    click and operation

--- script.js
+++ script.js
@@ -10,9 +10,6 @@ const numbers = document.querySelectorAll(".number")
 numbers.forEach((number) => {
     number.addEventListener("click", (event) => {
         console.log(event.target.value)
-       
-        // updateScreen(event.target.value)
-        // inputNumber(event.target.value)
     })
 })
 
@@ -21,7 +18,7 @@ let calculationOperator = ''
 let currentNumber = '0'
 
 const inputNumber = (number) => {
-    if (currentNumber === 0 ){
+    if (currentNumber === '0' ){
         currentNumber = number
     } else{
         currentNumber += number 
@@ -30,8 +27,8 @@ const inputNumber = (number) => {
 
 numbers.forEach((number) => {
     number.addEventListener("click", (event) => {       
-        updateScreen(currentNumber)
         inputNumber(event.target.value)
+        updateScreen(currentNumber)
     })
 })
 
@@ -42,8 +39,6 @@ operators.forEach((operator) => {
     operator.addEventListener("click", (event) => {
         inputOperator(event.target.value)
         // console.log('operator button is pressed')
-
-        // inputNumber(currentNumber)
     })
 })
 
@@ -62,7 +57,6 @@ const equalSign = document.querySelector(".equal-sign")
         calculate()
         updateScreen(currentNumber)
         // console.log('equal button is pressed')
-        // inputNumber(currentNumber)
     })
 
 const calculate = () => {
@@ -72,13 +66,13 @@ const calculate = () => {
             result = parseInt(prevNumber) + parseInt(currentNumber)
             break
         case "-":
-            result = prevNumber - currentNumber
+            result = parseInt(prevNumber) - parseInt(currentNumber)
             break
         case "*":
-            result = prevNumber * currentNumber
+            result = parseInt(prevNumber) * parseInt(currentNumber)
             break
         case "/":
-            result = prevNumber / currentNumber
+            result = parseInt(prevNumber) / parseInt(currentNumber)
             break
         default:
             break
@@ -86,4 +80,30 @@ const calculate = () => {
 
     currentNumber = result
     calculationOperator = ''
+}
+
+const clearBtn = document.querySelector(".all-clear")
+
+clearBtn.addEventListener("click", () => {
+    clearAll()
+    updateScreen(currentNumber)
+})
+const clearAll = () => {
+    prevNumber = ''
+    calculationOperator = ''
+    currentNumber = '0'
+}
+
+const decimal = document.querySelector(".decimal")
+
+decimal.addEventListener("click", (event) => {
+    inputDecimal(event.target.value)
+    updateScreen(currentNumber)
+})
+
+inputDecimal = (dot) => {
+    if (currentNumber.includes('.')) {
+        return
+    }
+    currentNumber += dot
 }
